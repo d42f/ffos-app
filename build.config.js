da48a8c4bb35cff00dd4e01ffa260729348130e7
d42f <dmitri.frolof@gmail.com>
2015-02-09 01:45:12 +0300

    bower dependencies

--- build.config.js
+++ build.config.js
@@ -62,11 +62,18 @@ module.exports = {
    */
   vendor_files: {
     js: [
+      'vendor/modernizr/modernizr.js',
+      'vendor/underscore/underscore.js',
+      'vendor/jquery/dist/jquery.js',
       'vendor/angular/angular.js',
       'vendor/angular-bootstrap/ui-bootstrap-tpls.min.js',
       'vendor/placeholders/angular-placeholders-0.0.1-SNAPSHOT.min.js',
       'vendor/angular-ui-router/release/angular-ui-router.js',
-      'vendor/angular-ui-utils/modules/route/route.js'
+      'vendor/angular-ui-utils/modules/route/route.js',
+      'vendor/restangular/src/restangular.js',
+      'vendor/angular-file-upload/angular-file-upload.js',
+      'vendor/ng-flow/dist/ng-flow-standalone.js',
+      'vendor/angular-once/once.js'
     ],
     css: [
     ],
